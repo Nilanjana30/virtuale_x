3f23443a8b6923d406c440847d3a8dd396a632b7
Nilanjan30 <ndey98919@gmail.com>
2024-03-04 16:48:57 +0530

    angle changee

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Logo from "../assets/logo.png";
 import Camera from "../assets/camera.svg";
 
@@ -6,42 +6,48 @@ function HomePage() {
   const data = [
     {
       id: 1,
-      img: require("../assets/main/img1.png"),
       assetImg: require("../assets/obj/obj1.png"),
-      angleImg: require("../assets/angleImgs/HighresScreenshot00022.png"),
+      angles: [
+        require("../assets/Sofa/ScreenShot000001.png"),
+        require("../assets/SofaAngle_1/HighresScreenshot00022.png"),
+        require("../assets/SofaAngle_2/HighresScreenshot00023.png"),
+      ],
     },
     {
       id: 2,
-      img: require("../assets/main/img2.png"),
       assetImg: require("../assets/obj/obj2.png"),
-      angleImg: require("../assets/angleImgs/HighresScreenshot00026.png"),
+      angles: [
+        require("../assets/Sofa/ScreenShot00012.png"),
+        require("../assets/SofaAngle_1/HighresScreenshot00026.png"),
+        require("../assets/SofaAngle_2/HighresScreenshot00027.png"),
+      ],
     },
+
     {
       id: 3,
-      img: require("../assets/main/img3.png"),
       assetImg: require("../assets/obj/obj3.png"),
-      angleImg: require("../assets/angleImgs/HighresScreenshot00025.png"),
+      angles: [
+        require("../assets/Sofa/ScreenShot00014.png"),
+        require("../assets/SofaAngle_1/HighresScreenshot00025.png"),
+        require("../assets/SofaAngle_2/HighresScreenshot00024.png"),
+      ],
     },
   ];
 
   const [active, setActive] = useState(data[0]);
   const [activeItem, setActiveItem] = useState(null);
-  const [angleImage, setAngleimage] = useState(false);
+  const [angleIndex, setAngleIndex] = useState(0);
 
-  const handleAngleimage = () => {
-    setAngleimage(!angleImage);
+  const handleCameraClick = () => {
+    setAngleIndex((prevIndex) => (prevIndex + 1) % active.angles.length);
   };
 
-  useEffect(() => {
-    setAngleimage(false);
-  }, [active]);
-
   return (
-    <div className="flex items-start justify-center h-screen">
+    <div className="flex items-start justify-center h-screen ">
       <div
-        className="flex justify-center items-end h-full w-3/4 bg-cover bg-no-repeat bg-center bg-[#717d88]"
+        className="flex justify-center items-end h-full w-3/4 bg-cover bg-no-repeat bg-center bg-[#717d88]  "
         style={{
-          backgroundImage: `url(${angleImage ? active.angleImg : active.img})`,
+          backgroundImage: `url(${active.angles[angleIndex]})`,
           transition: "background-image 0.5s ease",
         }}
       >
@@ -49,10 +55,14 @@ function HomePage() {
           src={Camera}
           alt="Camera Icon"
           className="w-20 h-20 pb-5 cursor-pointer"
-          onClick={handleAngleimage}
+          onClick={handleCameraClick}
         />
       </div>
-      <div className="flex flex-col justify-start items-center h-full w-1/4 bg-[#717d88]">
+
+      <div
+        className="flex flex-col justify-start items-center h-full w-1/4 bg-[#717d88]"
+        sm-w-12
+      >
         <div className="h-20 w-full bg-[#040835]  flex justify-center items-center ">
           <img src={Logo} alt="logo" className="w-52" />
         </div>
@@ -74,7 +84,7 @@ function HomePage() {
                 setActive(item);
               }}
             >
-              <img src={item.assetImg} alt="asset" loading="lazy" />
+              <img src={item.assetImg} alt="asset" />
             </div>
           );
         })}
