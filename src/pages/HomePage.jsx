358e9c9e70c59d78bbeb26390e804b320f417c35
Nilanjan30 <ndey98919@gmail.com>
2024-03-05 09:33:01 +0530

    add skeleton loading

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import Logo from "../assets/logo.png";
 import Camera from "../assets/camera.svg";
-import { LazyLoadImage } from "react-lazy-load-image-component";
-import "react-lazy-load-image-component/src/effects/blur.css";
+import { useState, useEffect } from "react";
 
-function HomePage() {
+const SkeletonLoader = () => {
+  return (
+    <div className="flex justify-center items-end h-full w-3/4 bg-cover bg-no-repeat bg-center bg-[#717d88] animate-pulse">
+      <div className="w-20 h-20 pb-5 bg-gray-300 rounded-full cursor-not-allowed"></div>
+    </div>
+  );
+};
+
+const HomePage = () => {
   const data = [
     {
       id: 1,
@@ -20,8 +27,8 @@ function HomePage() {
       assetImg: require("../assets/obj/obj2.png"),
       angles: [
         require("../assets/Sofa/ScreenShot00012.png"),
-        require("../assets/SofaAngle_1/HighresScreenshot00026.png"),
-        require("../assets/SofaAngle_2/HighresScreenshot00027.png"),
+        require("../assets/SofaAngle_1/HighresScreenshot00025.png"),
+        require("../assets/SofaAngle_2/HighresScreenshot00024.png"),
       ],
     },
 
@@ -30,8 +37,8 @@ function HomePage() {
       assetImg: require("../assets/obj/obj3.png"),
       angles: [
         require("../assets/Sofa/ScreenShot00014.png"),
-        require("../assets/SofaAngle_1/HighresScreenshot00025.png"),
-        require("../assets/SofaAngle_2/HighresScreenshot00024.png"),
+        require("../assets/SofaAngle_1/HighresScreenshot00026.png"),
+        require("../assets/SofaAngle_2/HighresScreenshot00027.png"),
       ],
     },
   ];
@@ -39,60 +46,69 @@ function HomePage() {
   const [active, setActive] = useState(data[0]);
   const [activeItem, setActiveItem] = useState(null);
   const [angleIndex, setAngleIndex] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleCameraClick = () => {
     setAngleIndex((prevIndex) => (prevIndex + 1) % active.angles.length);
   };
 
-  return (
-    <div className="flex items-start justify-center h-screen ">
-      <div
-        className="flex justify-center items-end h-full w-3/4 bg-cover bg-no-repeat bg-center bg-[#717d88]  "
-        style={{
-          backgroundImage: `url(${active.angles[angleIndex]})`,
-          transition: "background-image 0.5s ease",
-        }}
-      >
-        <LazyLoadImage
-          src={Camera}
-          alt="Camera Icon"
-          className="w-20 h-20 pb-5 cursor-pointer"
-          onClick={handleCameraClick}
-        />
-      </div>
+  useEffect(() => {
+    setIsLoading(true);
+    const image = new Image();
+    image.src = active.angles[angleIndex];
+    image.onload = () => {
+      setIsLoading(false);
+    };
+  }, [active, angleIndex]);
 
-      <div
-        className="flex flex-col justify-start items-center h-full w-1/4 bg-[#717d88]"
-        sm-w-12
-      >
+  return (
+    <div className="flex items-start justify-center h-screen">
+      {isLoading ? (
+        <SkeletonLoader />
+      ) : (
+        <div
+          className="flex justify-center items-end h-full w-3/4 bg-cover bg-no-repeat bg-center bg-[#717d88]  "
+          style={{
+            backgroundImage: `url(${active.angles[angleIndex]})`,
+            transition: "background-image 0.8s ease",
+            minHeight: "300px",
+          }}
+          loading="lazy"
+        >
+          <img
+            src={Camera}
+            alt="Camera Icon"
+            className="w-20 h-20 pb-5 cursor-pointer"
+            onClick={handleCameraClick}
+          />
+        </div>
+      )}
+
+      <div className="flex flex-col justify-start items-center h-full w-1/4 bg-[#717d88]">
         <div className="h-20 w-full bg-[#040835]  flex justify-center items-center ">
           <img src={Logo} alt="logo" className="w-52" />
         </div>
-        {data.map((item, i) => {
-          return (
-            <div
-              key={item.id}
-              className={`flex justify-center items-center w-48 h-48 mb-5 bg-[#040835] cursor-pointer ${
-                activeItem === item.id
-                  ? "border-solid border-4 border-white"
-                  : ""
-              }`}
-              style={{
-                marginTop: i === 0 ? "20px" : "0",
-              }}
-              onMouseEnter={() => setActiveItem(item.id)}
-              onMouseLeave={() => setActiveItem(null)}
-              onClick={() => {
-                setActive(item);
-              }}
-            >
-              <img src={item.assetImg} alt="asset" loading="lazy" />
-            </div>
-          );
-        })}
+        {data.map((item, i) => (
+          <div
+            key={item.id}
+            className={`flex justify-center items-center w-48 h-48 mb-5 bg-[#040835] cursor-pointer ${
+              activeItem === item.id ? "border-solid border-4 border-white" : ""
+            }`}
+            style={{
+              marginTop: i === 0 ? "20px" : "0",
+            }}
+            onMouseEnter={() => setActiveItem(item.id)}
+            onMouseLeave={() => setActiveItem(null)}
+            onClick={() => {
+              setActive(item);
+            }}
+          >
+            <img src={item.assetImg} alt="asset" />
+          </div>
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default HomePage;
