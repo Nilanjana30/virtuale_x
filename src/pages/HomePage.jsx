cbc34a4a4c9e942e4367ba2027fb1e25fc648661
Nilanjan30 <ndey98919@gmail.com>
2024-03-05 09:53:54 +0530

    add fixed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../assets/logo.png";
 import Camera from "../assets/camera.svg";
-import { useState, useEffect } from "react";
+import { useProgressiveImage } from "./Hooks/useProgressiveImage";
 
-const SkeletonLoader = () => {
-  return (
-    <div className="flex justify-center items-end h-full w-3/4 bg-cover bg-no-repeat bg-center bg-[#717d88] animate-pulse">
-      <div className="w-20 h-20 pb-5 bg-gray-300 rounded-full cursor-not-allowed"></div>
-    </div>
-  );
-};
-
-const HomePage = () => {
+function HomePage() {
   const data = [
     {
       id: 1,
@@ -46,32 +38,21 @@ const HomePage = () => {
   const [active, setActive] = useState(data[0]);
   const [activeItem, setActiveItem] = useState(null);
   const [angleIndex, setAngleIndex] = useState(0);
-  const [isLoading, setIsLoading] = useState(true);
 
   const handleCameraClick = () => {
     setAngleIndex((prevIndex) => (prevIndex + 1) % active.angles.length);
   };
 
-  useEffect(() => {
-    setIsLoading(true);
-    const image = new Image();
-    image.src = active.angles[angleIndex];
-    image.onload = () => {
-      setIsLoading(false);
-    };
-  }, [active, angleIndex]);
+  const loaded = useProgressiveImage(active.angles[angleIndex]);
 
   return (
-    <div className="flex items-start justify-center h-screen">
-      {isLoading ? (
-        <SkeletonLoader />
-      ) : (
+    <div className="flex items-start justify-center h-screen ">
+      {loaded ? (
         <div
           className="flex justify-center items-end h-full w-3/4 bg-cover bg-no-repeat bg-center bg-[#717d88]  "
           style={{
-            backgroundImage: `url(${active.angles[angleIndex]})`,
+            backgroundImage: `url(${loaded})`,
             transition: "background-image 0.8s ease",
-            minHeight: "300px",
           }}
           loading="lazy"
         >
@@ -82,33 +63,50 @@ const HomePage = () => {
             onClick={handleCameraClick}
           />
         </div>
+      ) : (
+        <div
+          className="flex justify-center items-end h-full w-3/4 bg-cover bg-no-repeat bg-center bg-[#717d88]  "
+          style={{
+            transition: "background-image 0.8s ease",
+          }}
+          loading="lazy"
+        >
+          Loading...........................
+        </div>
       )}
 
-      <div className="flex flex-col justify-start items-center h-full w-1/4 bg-[#717d88]">
+      <div
+        className="flex flex-col justify-start items-center h-full w-1/4 bg-[#717d88]"
+        sm-w-12
+      >
         <div className="h-20 w-full bg-[#040835]  flex justify-center items-center ">
           <img src={Logo} alt="logo" className="w-52" />
         </div>
-        {data.map((item, i) => (
-          <div
-            key={item.id}
-            className={`flex justify-center items-center w-48 h-48 mb-5 bg-[#040835] cursor-pointer ${
-              activeItem === item.id ? "border-solid border-4 border-white" : ""
-            }`}
-            style={{
-              marginTop: i === 0 ? "20px" : "0",
-            }}
-            onMouseEnter={() => setActiveItem(item.id)}
-            onMouseLeave={() => setActiveItem(null)}
-            onClick={() => {
-              setActive(item);
-            }}
-          >
-            <img src={item.assetImg} alt="asset" />
-          </div>
-        ))}
+        {data.map((item, i) => {
+          return (
+            <div
+              key={item.id}
+              className={`flex justify-center items-center w-48 h-48 mb-5 bg-[#040835] cursor-pointer ${
+                activeItem === item.id
+                  ? "border-solid border-4 border-white"
+                  : ""
+              }`}
+              style={{
+                marginTop: i === 0 ? "20px" : "0",
+              }}
+              onMouseEnter={() => setActiveItem(item.id)}
+              onMouseLeave={() => setActiveItem(null)}
+              onClick={() => {
+                setActive(item);
+              }}
+            >
+              <img src={item.assetImg} alt="asset" />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
-};
+}
 
 export default HomePage;
